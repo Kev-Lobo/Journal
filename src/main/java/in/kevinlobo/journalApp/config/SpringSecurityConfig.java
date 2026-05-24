ababf2d6af20cddf6d007c283fe486ca06d70d0c
kevinlobo <kevinlobo998@gmail.com>
2026-05-24 15:41:39 +0530

    Implemeting Spring Security into the project to

---
+++ src/main/java/in/kevinlobo/journalApp/config/SpringSecurityConfig.java
@@ -0,0 +1,15 @@
+package in.kevinlobo.journalApp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+    }
+}
