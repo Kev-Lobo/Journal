432dd039739ac3a8986360c25b70dcd936642b86
kevinlobo <kevinlobo998@gmail.com>
2026-05-26 23:34:00 +0530

    Implemeting Spring Security into the project wrote a SpringSecurityConfig class to authorize requests and implement form login.

--- src/main/java/in/kevinlobo/journalApp/config/SpringSecurityConfig.java
+++ src/main/java/in/kevinlobo/journalApp/config/SpringSecurityConfig.java
@@ -1,15 +1,38 @@
 package in.kevinlobo.journalApp.config;
 
+import in.kevinlobo.journalApp.service.UserServiceDetailsImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private UserServiceDetailsImpl userDetailsService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+       http.authorizeRequests()
+               .antMatchers("/journal/**").authenticated()
+               .anyRequest().permitAll()
+               .and()
+               .httpBasic();
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
     }
 }
--- src/main/java/in/kevinlobo/journalApp/entity/User.java
+++ src/main/java/in/kevinlobo/journalApp/entity/User.java
@@ -26,4 +26,6 @@ public class User {
 
     @DBRef // this annotation will create the reference of the JournalEntries table(class) in to users table(table)
     private List<JournalEntry> journalEntries = new ArrayList<>();
+
+    private List<String> roles;
 }
