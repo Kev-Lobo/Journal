5ec4160cfb1800d6b137990935262f37e73f432a
kevinlobo <kevinlobo998@gmail.com>
2026-06-10 11:41:59 +0530

    creating test cases for Service classes

--- src/test/java/in/kevinlobo/journalApp/service/UserServiceDetailsImplTest.java
+++ src/test/java/in/kevinlobo/journalApp/service/UserServiceDetailsImplTest.java
@@ -1,19 +1,14 @@
 package in.kevinlobo.journalApp.service;
 
-import in.kevinlobo.journalApp.entity.User;
 import in.kevinlobo.journalApp.repository.UserRepository;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.core.userdetails.UserDetails;
-
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -34,31 +29,8 @@ public class UserServiceDetailsImplTest {
 
     @Test
     void loadUserByUserNameTest() {
-//        when(userRepository.findByUserName(ArgumentMatchers.anyString()))
-//                .thenReturn(org.springframework.security.core.userdetails.User.builder().username("kevin").password("kevin").roles().build());
-//        UserDetails userDetails = userServiceDetailsImpl.loadUserByUsername("kevin");
-//        Assertions.assertNotNull(userDetails);
-        User mockUser;
-        mockUser = new User();
-        mockUser.setUserName("testUser");
-        mockUser.setPassword("encodedPassword");
-        mockUser.setRoles(List.of("USER", "ADMIN"));
 
-        when(userRepository.findByUserName("testUser")).thenReturn(mockUser);
-
-        // Act
-        UserDetails userDetails = userServiceDetailsImpl.loadUserByUsername("testUser");
-
-        // Assert
+        UserDetails userDetails = userServiceDetailsImpl.loadUserByUsername("kevin");
         assertNotNull(userDetails);
-        assertEquals("testUser", userDetails.getUsername());
-        assertEquals("encodedPassword", userDetails.getPassword());
-        assertTrue(userDetails.getAuthorities().stream()
-                .anyMatch(a -> a.getAuthority().equals("ROLE_USER")));
-        assertTrue(userDetails.getAuthorities().stream()
-                .anyMatch(a -> a.getAuthority().equals("ROLE_ADMIN")));
-
-        verify(userRepository).findByUserName("testUser");
-        verifyNoMoreInteractions(userRepository);
     }
 }
