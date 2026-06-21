96198c5d2eb59fe3ac7d6f204acec3dec6341399
kevinlobo <kevinlobo998@gmail.com>
2026-06-21 23:46:41 +0530

    Learning how to use MongoTemplate using the Query class and criteria methods

---
+++ src/main/java/in/kevinlobo/journalApp/repository/UserRepositoryImpl.java
@@ -0,0 +1,26 @@
+package in.kevinlobo.journalApp.repository;
+
+import in.kevinlobo.journalApp.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.apache.coyote.http11.Constants.a;
+
+public class UserRepositoryImpl {
+
+
+    private MongoTemplate mongoTemplate;
+
+    public List<User> getUsersForSA() {
+        Query query = new Query();
+        query.addCriteria(Criteria.where("email").regex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$"));
+        query.addCriteria(Criteria.where("sentimentAnalysis").is(true));
+        List<User> users = mongoTemplate.find(query, User.class);
+        return users ;
+    }
+}
