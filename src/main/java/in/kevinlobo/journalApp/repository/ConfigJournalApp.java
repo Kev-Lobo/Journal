1855a00be3de101f2f3e6c005b8a014d04363787
kevinlobo <kevinlobo998@gmail.com>
2026-06-26 23:55:50 +0530

    changed the logging file address and some other changes

--- src/main/java/in/kevinlobo/journalApp/repository/ConfigJournalApp.java
+++ src/main/java/in/kevinlobo/journalApp/repository/ConfigJournalApp.java
@@ -1,7 +1,6 @@
 package in.kevinlobo.journalApp.repository;
 
 import in.kevinlobo.journalApp.entity.ConfigJournalAppEntity;
-import in.kevinlobo.journalApp.entity.User;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
