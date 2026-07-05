07b57a5fdadbe1edccf142330150f5cfdbe7cccd
kevinlobo <kevinlobo998@gmail.com>
2026-07-05 22:32:42 +0530

    ..

--- src/main/java/in/kevinlobo/journalApp/service/UserDetailsServiceImpl.java
+++ src/main/java/in/kevinlobo/journalApp/service/UserDetailsServiceImpl.java
@@ -13,10 +13,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
-
-
-
-
     @Autowired
     private UserRepository userRepository;
 
